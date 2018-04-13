fb5976934975a8ba884da69ea42c0efb88cf60cf
Viktor <Likin>
2018-04-13 19:00:04 +0300

    The first staff.

--- src/index.js
+++ src/index.js
@@ -7,12 +7,38 @@ let srt = fs.readFileSync('./data/test.srt', 'utf-8');
 var data = parser.fromSrt(srt);
 let text = _.join(_.map(data, 'text'), ' ');
 
-let words = new pos.Lexer().lex('This is some sample text. This text can contain multiple sentences.');
+let words = new pos.Lexer().lex(text);
 let tagger = new pos.Tagger();
 let taggedWords = tagger.tag(words);
-for (let i in taggedWords) {
-  var taggedWord = taggedWords[i];
-  var word = taggedWord[0];
-  var tag = taggedWord[1];
-  console.log(word + " /" + tag);
-}
+
+// let u =
+//   _.join(
+//     _.map(
+//       _.sortBy(
+//         _.uniqBy(
+//           taggedWords,
+//           0
+//         ),
+//         0
+//       ),
+//       0
+//     ),
+//     ' '
+// );
+
+let u = _.chain(taggedWords)
+  .map(0)
+  .map(a => a.toLowerCase())
+  .uniq()
+  .sort()
+  .chunk(10)
+  .map((chunk) => _.join(chunk, ' '))
+  .join('\n')
+  .value();
+console.log(u);
+// for (let i in taggedWords) {
+//   var taggedWord = taggedWords[i];
+//   var word = taggedWord[0];
+//   var tag = taggedWord[1];
+//   console.log(word + " /" + tag);
+// }
